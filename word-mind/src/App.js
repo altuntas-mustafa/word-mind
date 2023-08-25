5659f03f70a7108421cf5ee85952315cc9012cfc
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-25 13:49:42 +0200

    chatgpt api works

--- word-mind/src/App.js
+++ word-mind/src/App.js
@@ -40,7 +40,7 @@ function App() {
         <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"to={"/createdecks"}> Create Deck </Link>
       </li>
       <li>
-        <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" to={"/languages/:language/decks/:deckName"} >Flash Cards </Link>
+        <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" to={"/chatgpt"} >Chat Gpt </Link>
       </li>
       
     </ul>
