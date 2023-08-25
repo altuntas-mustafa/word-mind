83ab39fd8932baddd6ed7b3335661718c4a7d370
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-25 11:23:09 +0200

    project stting is done

---
+++ word-mind/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-blue-500 text-white p-4">
+        This is a styled div.
+      </div>
+    </div>
+  );
+}
+
+export default App;
