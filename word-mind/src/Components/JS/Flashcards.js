3b1c5880e8bc10297bd78febcab203e07ebe106d
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-28 16:15:58 +0200

    some bugs fixed

--- word-mind/src/Components/JS/Flashcards.js
+++ word-mind/src/Components/JS/Flashcards.js
@@ -74,25 +74,30 @@ const Flashcards = () => {
 
   return (
     <div className="flex-col justify-center items-center min-h-screen">
-      <h2 className="font-serif text-x mb-4 text-center justify-center">{deckName}</h2>
+      <h2 className="font-serif text-x mb-4 text-center justify-center">
+        {deckName}
+      </h2>
+      
       <div className="w-full mt-20 bg-white flex flex-col items-center text-3xl">
   {!isFlipped ? (
     <div className="mb-3 font-normal text-gray-900 dark:text-gray-700 flex flex-col items-center justify-center h-[300px] w-[400px] overflow-hidden">
-      <div className="mb-3 font-normal text-gray-900 dark:text-gray-700 items-center text-center overflow-hidden">
-        {isFrontDisplayed && Math.random() < 0.5 ? (
-          <p className="whitespace-pre-wrap">{currentFlashcard.back}</p>
-        ) : (
-          <p className="whitespace-pre-wrap">{currentFlashcard.front}</p>
-        )}
+      <div className="mb-3 font-normal text-gray-900 dark:text-gray-700 items-center text-center overflow-hidden flex-grow">
+        <div className="flex items-center justify-center h-full">
+          {isFrontDisplayed && Math.random() < 0.5 ? (
+            <p className="whitespace-pre-wrap">{currentFlashcard.back}</p>
+          ) : (
+            <p className="whitespace-pre-wrap">{currentFlashcard.front}</p>
+          )}
+        </div>
       </div>
     </div>
   ) : (
-    <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex flex-col items-center justify-center h-[300px] w-[400px] overflow-hidden">
-      <p className="mb-1 font-normal text-gray-700 dark:text-gray-400 font-serif whitespace-pre-wrap">
+    <div className="mb-3 font-normal text-gray-900 dark:text-gray-400 flex flex-col items-center justify-center h-[300px] w-[400px] overflow-hidden">
+      <p className="mb-1 font-normal text-gray-900 dark:text-gray-400 font-serif whitespace-pre-wrap">
         {currentFlashcard.front}
       </p>
-      <hr className="w-full h-0.5 bg-black my-3" />
-      <p className="mb-1 font-normal text-gray-700 dark:text-gray-400 font-serif whitespace-pre-wrap">
+      <hr className="w-screen h-0.5 bg-black my-3 border-none" />
+      <p className="mb-1 font-normal text-gray-900 dark:text-gray-400 font-serif whitespace-pre-wrap">
         {currentFlashcard.back}
       </p>
     </div>
@@ -100,11 +105,10 @@ const Flashcards = () => {
 </div>
 
 
-      {/* Button section */}
-      <div className="w-full mt-20 bg-white flex flex-col items-center text-3xl">
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-4 mt-4">
         {!isFlipped ? (
           <button
-            className="px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-24"
             onClick={handleFlip}
           >
             Flip
@@ -112,19 +116,19 @@ const Flashcards = () => {
         ) : isLastFlashcard ? (
           <Link
             to="/decks"
-            className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-24"
           >
             Go to Decks
           </Link>
         ) : (
           <button
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-24"
             onClick={handleNextCard}
           >
             Next Card
           </button>
         )}
-    </div>
+      </div>
     </div>
   );
 };
