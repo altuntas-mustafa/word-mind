3fbd1ef561f263b5250a646c7a6183cda8fa2a4f
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-28 14:53:11 +0200

    changes

--- word-mind/src/Components/JS/Header.jsx
+++ word-mind/src/Components/JS/Header.jsx
@@ -7,7 +7,6 @@ export default function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // Check the user's authentication status on component mount
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -28,10 +27,68 @@ export default function Header() {
         </a>
         <nav>
           <section className="lg:hidden">
-            {/* ... (rest of your code) */}
+            <div
+              className="space-y-2"
+              onClick={() => setIsNavOpen((prev) => !prev)}
+            >
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            </div>
+
             {isNavOpen && (
               <div className="fixed top-0 left-0 w-full h-full bg-white z-10 flex flex-col justify-evenly items-center">
-                {/* ... (rest of your code) */}
+                <div
+                  className="absolute top-0 right-0 px-8 py-8"
+                  onClick={() => setIsNavOpen(false)}
+                >
+                  <svg
+                    className="h-8 w-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <ul className="flex flex-col items-center justify-between min-h-[250px] bg-white backdrop-blur-md rounded-lg p-6 space-y-4">
+                  <li>
+                    <a href="/" className="text-lg text-gray-700 hover:text-yellow-300">Home</a>
+                  </li>
+                  <li>
+                    <a href="/decks" className="text-lg text-gray-700 hover:text-yellow-300">Decks</a>
+                  </li>
+                  <li>
+                    <a href="/createdecks" className="text-lg text-gray-700 hover:text-yellow-300">Create Deck</a>
+                  </li>
+                  <li>
+                    <a href="/dashboard" className="text-lg text-gray-700 hover:text-yellow-300">DashBoard</a>
+                  </li>
+                  {/* Conditionally show login or logout link */}
+                  {isLoggedIn ? (
+                    <li>
+                      <Link
+                        to="/logout"
+                        className="text-lg text-gray-700 hover:text-yellow-300"
+                      >
+                        Logout
+                      </Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link
+                        to="/login"
+                        className="text-lg text-gray-700 hover:text-yellow-300"
+                      >
+                        Login
+                      </Link>
+                    </li>
+                  )}
+                </ul>
               </div>
             )}
           </section>
