12897bb24e2aa9b0db459144018baa7e7288ee54
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-09-07 13:46:20 +0200

    button bugs fixed

--- word-mind/src/JS/SeeDeck.js
+++ word-mind/src/JS/SeeDeck.js
@@ -78,74 +78,75 @@ const SeeDeck = () => {
         {console.log(deckFlashcards)}
         {deckName}
         <div className="flex items-center space-x-2 ml-8">
-          {currentUser && creator === "creator" ? (
-            <>
-              <button
-                onClick={() => {
-                  try {
-                    handleLikeClick(language, deckName);
-                  } catch (error) {
-                    console.error("Error while adding/deleting deck:", error);
-                  }
-                }}
-                className={`px-4 py-2 rounded-full font-semibold ${
-                  localIsLiked
-                    ? "bg-green-400 text-white"
-                    : "bg-gray-200 text-gray-700"
-                } hover:bg-opacity-80 transition-colors duration-300 flex items-center space-x-2`}
-              >
-                {localIsLiked ? (
-                  <>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="1em"
-                      viewBox="0 0 448 512"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-                      />
-                    </svg>
-                    <div className="text-sm">Liked</div>
-                  </>
-                ) : (
-                  <>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="1em"
-                      viewBox="0 0 512 512"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0-14 10.7 24-24s24 10.7 24 24H280v64c0-13.3-10.7-24-24-24s-14-10.7 24-24z"
-                      />
-                    </svg>
-                    <div className="text-sm">Like</div>
-                  </>
-                )}
-              </button>
-              <button
-                onClick={() => {
-                  handleDeleteClick(language, deckName, false);
-                }}
-                className="px-4 py-2 rounded-full font-semibold bg-red-500 text-white hover:bg-red-600 transition-colors duration-300 flex items-center space-x-2"
-              >
-                <div className="text-sm">X DELETE</div>
-              </button>
-            </>
-          ) : currentUser && userId ? (
-            <button
-              onClick={() => {
-                handleDeleteClick(language, deckName, true);
-              }}
-              className="px-4 py-2 rounded-full font-semibold bg-red-500 text-white hover:bg-red-600 transition-colors duration-300 flex items-center space-x-2"
+  {currentUser && creator === "creator" ? (
+    <>
+      <button
+        onClick={() => {
+          try {
+            handleLikeClick(language, deckName);
+          } catch (error) {
+            console.error("Error while adding/deleting deck:", error);
+          }
+        }}
+        className={`px-4 py-2 rounded-full font-semibold ${
+          localIsLiked
+            ? "bg-green-400 text-white"
+            : "bg-gray-200 text-gray-700"
+        } hover:bg-opacity-80 transition-colors duration-300 flex items-center space-x-2`}
+      >
+        {localIsLiked ? (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+              className="w-5 h-5"
             >
-              <div className="text-sm">X DELETE</div>
-            </button>
-          ) : null}
-        </div>
+              <path
+                fill="currentColor"
+                d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+              />
+            </svg>
+            <div className="text-sm">Liked</div>
+          </>
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"
+              className="w-5 h-5"
+            >
+              <path
+                fill="currentColor"
+                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0-14 10.7 24-24s24 10.7 24 24H280v64c0-13.3-10.7-24-24-24s-14-10.7 24-24z"
+              />
+            </svg>
+            <div className="text-sm">Like</div>
+          </>
+        )}
+      </button>
+      <button
+        onClick={() => {
+          handleDeleteClick(language, deckName, false);
+        }}
+        className="px-4 py-2 rounded-full font-semibold bg-red-500 text-white hover:bg-red-600 transition-colors duration-300 flex items-center space-x-2"
+      >
+        <div className="text-sm">X DELETE</div>
+      </button>
+    </>
+  ) : currentUser && userId ? (
+    <button
+      onClick={() => {
+        handleDeleteClick(language, deckName, true);
+      }}
+      className="px-4 py-2 rounded-full font-semibold bg-red-500 text-white hover:bg-red-600 transition-colors duration-300 flex items-center space-x-2"
+    >
+      <div className="text-sm">X DELETE</div>
+    </button>
+  ) : null}
+</div>
+
       </h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {deckFlashcards.map((flashcard, index) => (
